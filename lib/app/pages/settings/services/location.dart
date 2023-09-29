f0894365bc35a10e5292093996cd69e0e59bbe4d
musayounus <musayounus95@gmail.com>
2023-09-30 00:15:50 +0300

    fixed location

--- lib/app/pages/settings/services/location.dart
+++ lib/app/pages/settings/services/location.dart
@@ -1,10 +1,29 @@
 import 'package:geolocator/geolocator.dart';
 
 class Location {
-  late double latitude;
-  late double longitude;
-
+  double? latitude;
+  double? longitude;
   Future<void> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
     try {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.low);
@@ -15,3 +34,21 @@ class Location {
     }
   }
 }
+
+// import 'package:geolocator/geolocator.dart';
+//
+// class Location {
+//   late double latitude;
+//   late double longitude;
+//
+//   Future<void> getCurrentLocation() async {
+//     try {
+//       Position position = await Geolocator.getCurrentPosition(
+//           desiredAccuracy: LocationAccuracy.low);
+//       latitude = position.latitude;
+//       longitude = position.longitude;
+//     } catch (e) {
+//       print(e);
+//     }
+//   }
+// }
